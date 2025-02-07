1e453b4606ecde30c340437e3eb88201530848f1
md <mdchristien12@gmail.com>
2025-02-07 10:16:14 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Test from "@/features/Test";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex gap-4 p-10">
+      <Button  >Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="teritary">Teritary</Button>
+    </div>
+  );
+}
---
+++ src/features/Test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className='text-red-500 text-2xl'>Test components</div>
+  )
+}
+
+export default Test
