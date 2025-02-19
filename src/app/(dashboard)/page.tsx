d95be6139e84c249e1c7f59e04281e9ca6243143
md <mdchristien12@gmail.com>
2025-02-19 10:57:10 +0100

    redirect user to current workspace

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,15 +2,16 @@ import { getCurrent } from "@/features/auth/actions";
 import UserButton from "@/features/auth/components/user-button";
 import CreateWorkSpaceForm from "@/features/workspaces/components/CreateWorkSpaceForm";
 import { redirect } from "next/navigation";
+import { getWorkspaces } from "./workspaces/actions";
 
 export default async function Home() {
   const user = await getCurrent();
-
   if (!user) redirect("/sign-in");
 
-  return (
-    <div className="">
-      <CreateWorkSpaceForm />
-    </div>
-  );
+  const workspaces = await getWorkspaces();
+  if (workspaces.total === 0) {
+    redirect("/workspaces/create");
+  } else {
+    redirect(`/workspaces/${workspaces.documents[0].$id}`);
+  }
 }
