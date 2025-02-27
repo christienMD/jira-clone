85f0694fb8393b4f128a869cacb88358907c72d1
md <mdchristien12@gmail.com>
2025-02-27 17:02:32 +0100

    feat: Filter tasks by date

--- src/features/tasks/components/DataFilters.tsx
+++ src/features/tasks/components/DataFilters.tsx
@@ -9,9 +9,10 @@ import {
 import { useGetMembers } from "@/features/members/api/useGetMembers";
 import { useGetProjects } from "@/features/projects/api/useGetProjects";
 import useWorkSpaceId from "@/features/workspaces/hooks/useWorkSpaceId";
-import { ListCheckIcon } from "lucide-react";
+import { FolderIcon, ListCheckIcon, UserIcon } from "lucide-react";
 import { TaskStatus } from "../types";
 import { useTasFilters } from "../hooks/useTaskFilters";
+import DatePicker from "@/components/DatePicker";
 
 interface Props {
   hideProjectFiler?: boolean;
@@ -46,11 +47,23 @@ const DataFilters = ({ hideProjectFiler }: Props) => {
     setFilters({ status: value === "all" ? null : (value as TaskStatus) });
   };
 
+  const onAssigneeChange = (value: string) => {
+    setFilters({ assigneeId: value === "all" ? null : (value as string) });
+  };
+
+  const onProjectChange = (value: string) => {
+    setFilters({ projectId: value === "all" ? null : (value as string) });
+  };
+
   if (isLoading) return null;
 
   return (
     <div className="flex flex-col lg:flex-row gap-2">
-      <Select defaultValue={status ?? undefined} onValueChange={(value) => onStatusChange(value)}>
+      {/* status */}
+      <Select
+        defaultValue={assigneeId ?? undefined}
+        onValueChange={(value) => onStatusChange(value)}
+      >
         <SelectTrigger className="w-full lg:w-auto h-8">
           <div className="flex items-center pr-2">
             <ListCheckIcon className="size-4 mr-2" />
@@ -67,6 +80,58 @@ const DataFilters = ({ hideProjectFiler }: Props) => {
           <SelectItem value={TaskStatus.DONE}>Done</SelectItem>
         </SelectContent>
       </Select>
+
+      {/* assignee  */}
+      <Select
+        defaultValue={status ?? undefined}
+        onValueChange={(value) => onAssigneeChange(value)}
+      >
+        <SelectTrigger className="w-full lg:w-auto h-8">
+          <div className="flex items-center pr-2">
+            <UserIcon className="size-4 mr-2" />
+            <SelectValue placeholder="All assignees" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">All assignees</SelectItem>
+          <SelectSeparator />
+          {memberOptions?.map((member) => (
+            <SelectItem key={member.value} value={member.value}>
+              {member.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      {/* projects */}
+      <Select
+        defaultValue={projectId ?? undefined}
+        onValueChange={(value) => onProjectChange(value)}
+      >
+        <SelectTrigger className="w-full lg:w-auto h-8">
+          <div className="flex items-center pr-2">
+            <FolderIcon className="size-4 mr-2" />
+            <SelectValue placeholder="All projects" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">All projects</SelectItem>
+          <SelectSeparator />
+          {projectOptions?.map((project) => (
+            <SelectItem key={project.value} value={project.value}>
+              {project.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <DatePicker
+        placeholder="Due date"
+        className="h-8 w-full lg:w-auto"
+        value={dueDate ? new Date(dueDate) : undefined}
+        onChange={(date) => {
+          setFilters({ dueDate: date ? date.toISOString() : null });
+        }}
+      />
     </div>
   );
 };
